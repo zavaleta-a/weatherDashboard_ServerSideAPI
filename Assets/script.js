6e9d53e57492dd9e51d979dfdeaa01c43e47cea8
Alfredo Zavaleta <zavaleta@Alfredos-MBP.attlocal.net>
2021-09-05 20:49:12 -0500

    created variable for api call

--- Assets/script.js
+++ Assets/script.js
@@ -1,2 +1,8 @@
 // creating a variable to save API Key for future use
 var APIKey = "a4594417e70fc201979b8f330a80381f"
+
+// creating a variable for the city we want the weather for
+var city; 
+
+// creating queryURL for API call
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" +city + "&appid=" + APIKey;
