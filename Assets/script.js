7ff256e7681b072e1d2d7210dbc9d74e8ab1f85e
Alfredo Zavaleta <zavaleta@Alfredos-MacBook-Pro.local>
2021-09-07 15:49:49 -0500

    added jquery src in HTML

--- Assets/script.js
+++ Assets/script.js
@@ -2,10 +2,14 @@
 var APIKey = "a4594417e70fc201979b8f330a80381f"
 
 // creating a variable for the city we want the weather for
-var city; 
+var city;
+
+// creating a variable for the past cities
+var savedLocations = [];
 
 // creating queryURL for API call
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" +city + "&appid=" + APIKey;
 
 // creating a fetch to call the API
 fetch(queryURL)
+
